d66e2d606bc163a3bfae53ed0fe95ce36feee2f9
rajashreem <rajashreermalvade@gmail.com>
2011-10-03 15:13:42 +0530

    waste commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,6 @@
 class User < ActiveRecord::Base
 
-	attr_accessor :password
-	attr_accessible :name, :email, :password, :password_confirmation
+	attr_accessible :name, :email
 
-
-	regex = /\A[\w+\-.]+@[a-z\-\d.]+\.[a-z]+\z/i
-	
-	validates :name, :presence => true,
-				:length => {:maximum => 50}
-					
-
-	validates :email, :presence => true,
-				:format=> {:with => regex},
-				:uniqueness => {:case_sensitive => false}
-
-	validates :password, :presence => true,
-					:confirmation => true,
-					:length => {:within => 6..39}
-
-	before_save :encrypt_password
-
-	def has_password? (given_password) 
-	end
-
-	private
-	
-		def encrypt_password
-			self.encrypted_password = encrypt(password)
-		end
-
-		def encrypt(string)
-			string
-		end
+	validates :name, presence => true
 end
